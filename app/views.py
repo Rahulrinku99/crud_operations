0bad1857fec2dac0966c6cca08739bab8ddd7ba1
Rahulrinku99 <rahul7829794470@gmail.com>
2023-04-07 14:56:57 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from app.models import *
+
+# Create your views here.
+def display_topic(request):
+    LOT=Topic.objects.all()
+    d={'topics':LOT}
+    return render(request,'display_topic.html',d)
+
+
+def display_webpage(request):
+    LOW=Webpage.objects.all()
+    d={'webpages':LOW}
+    return render(request,'display_webpage.html',d)
+    
+def display_access(request):
+    LOA=AccessRecord.objects.all()
+    d={'access':LOA}
+    return render(request,'display_access.html',d)
